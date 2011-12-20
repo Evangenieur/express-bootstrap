95cf001d2cd97018cf8b581386fa5c9bd22aa314
bradley griffiths <bradley.griffiths@gmail.com>
2011-12-20 23:59:40 +0000

    moved some requires

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-var mongoose = require('mongoose');
-var express = require('express');
-var stylus = require('stylus');
-var app = express.createServer();
-var models = {};
-var config = require('./lib/config.js')(express, app, mongoose, models, stylus);
-var helpers = require('./lib/helpers.js');
-var routes = require('./lib/routes.js')(helpers, config, app, models);
+var setup = require('./lib/setup.js'),
+    port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000);
+require('./lib/routes.js')(setup);
+
+setup.app.listen(port);
